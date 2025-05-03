a96fbda0fa1b38c6560e739da123e898028d2e66
GVHemanth <venkatahemanth135@gmail.com>
2025-05-03 20:38:10 +0530

    Implemented view models

--- JobApp/src/main/java/com/example/JobApp/controller/JobController.java
+++ JobApp/src/main/java/com/example/JobApp/controller/JobController.java
@@ -2,17 +2,25 @@ package com.example.JobApp.controller;
 
 
 import com.example.JobApp.model.JobPost;
+import com.example.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
-@Controller
+@RestController
+//@Controller
 public class JobController {
 
-    JobPost jobPost;
+//    JobPost jobPost;
+
+    @Autowired
+    JobService jobService;
 
     @GetMapping({"/", "home"})
     public String home(){
@@ -29,12 +37,18 @@ public class JobController {
     @PostMapping("handleForm")
     public String handleForm(JobPost jobPost){
 
-        return "success";
+        jobService.addJob(jobPost);
+
+
+        return "successa";
     }
 
     @GetMapping("viewalljobs")
-    public String viewAllJobs(){
-
+    public String viewAllJobs(Model model){
+//    public List<JobPost> viewAllJobs(Model model){
+        List<JobPost> jobs = jobService.getAlljobs();
+        model.addAttribute("jobPosts", jobs);
         return "viewalljobs";
+//        return jobs;
     }
 }
