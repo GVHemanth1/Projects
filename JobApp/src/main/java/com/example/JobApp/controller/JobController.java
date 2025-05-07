e260543d09ed9addfa36e8c5e30a870a5c1e2ba8
GVHemanth <venkatahemanth135@gmail.com>
2025-05-07 20:13:25 +0530

    adding changes

--- JobApp/src/main/java/com/example/JobApp/controller/JobController.java
+++ JobApp/src/main/java/com/example/JobApp/controller/JobController.java
@@ -1,14 +1,11 @@
 package com.example.JobApp.controller;
 
-
 import com.example.JobApp.model.JobPost;
 import com.example.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
