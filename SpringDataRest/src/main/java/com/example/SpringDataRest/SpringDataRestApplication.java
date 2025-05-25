274cad6bc6d03bcb20b8b78a61f012599e7fb7ef
GVHemanth <venkatahemanth135@gmail.com>
2025-05-25 23:39:01 +0530

    changes added

--- SpringDataRest/src/main/java/com/example/SpringDataRest/SpringDataRestApplication.java
+++ SpringDataRest/src/main/java/com/example/SpringDataRest/SpringDataRestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDataRestApplication {
 
+//	Running REST services without any controller
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataRestApplication.class, args);
 	}
