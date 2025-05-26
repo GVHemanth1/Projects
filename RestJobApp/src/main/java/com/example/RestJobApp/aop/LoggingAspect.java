51eeac19138312f8c6c7eb681d110384fb6fb70a
GVHemanth <venkatahemanth135@gmail.com>
2025-05-26 22:09:44 +0530

    Spring AOP added

---
+++ RestJobApp/src/main/java/com/example/RestJobApp/aop/LoggingAspect.java
@@ -0,0 +1,30 @@
+package com.example.RestJobApp.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+//This is Aspect
+public class LoggingAspect {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+// @After is Advice and ("execution(* com.example.RestJobApp.service.JobService.getAlljobs(..))")
+//    is Pointcut
+    @Before("execution(* com.example.RestJobApp.service.JobService.*(..))")
+    public void logMethodCall(JoinPoint joinPoint){
+        LOGGER.info("Method called : " + joinPoint.getSignature().getName());
+    }
+
+// Multiple point cut can be used by ||
+    @After("execution(* com.example.RestJobApp.service.JobService.getAlljobs(..)) || execution(* com.example.RestJobApp.service.JobService.getjob(..))")
+    public void logMethodExecuted(JoinPoint joinPoint){
+        LOGGER.info("Method Executed : " + joinPoint.getSignature().getName());
+    }
+}
--- SpringDataRest/src/main/java/com/example/SpringDataRest/repo/JobRepo.java
+++ SpringDataRest/src/main/java/com/example/SpringDataRest/repo/JobRepo.java
@@ -3,10 +3,12 @@ package com.example.SpringDataRest.repo;
 
 import com.example.SpringDataRest.model.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface JobRepo extends JpaRepository<JobPost,Integer> {
+
 
 }
